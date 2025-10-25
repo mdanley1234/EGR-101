bee147d3dd119b43a247a484ed4b7aa12314b57a
Harshith Guduru <83043028+harshith998@users.noreply.github.com>
2025-10-25 01:01:01 -0400

    added stuff for simpel test prototype

--- software/ESP32/src/simpleSensorTesting.cpp
+++ software/ESP32/src/simpleSensorTesting.cpp
@@ -1,8 +1,10 @@
 #include <Wire.h>
 #include "Adafruit_VEML7700.h"
+#include <LiquidCrystal_I2C.h>
 
 Adafruit_VEML7700 veml = Adafruit_VEML7700();
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const unsigned long READ_INTERVAL_MS = 200; // how often to sample (ms)
 const float MAX_LUX_FOR_PWM = 100000.0; // map lux 0..MAX_LUX_FOR_PWM -> PWM 0..255
@@ -19,6 +21,13 @@ void setup() {
     while (1) { delay(1000); }
   }
   Serial.println(F("VEML7700 found."));
+
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  lcd.setCursor(0,0);
+  lcd.print("Light Intensity (pwmR, rawL):");
 }
 
 void loop() {
@@ -31,7 +40,7 @@ void loop() {
 
   // For the purpose of plotting we will map lux to a 0..255 PWM value linearly.
   float clampLux = lux;
-  if (!isFinite(clampLux) || clampLux < 0) clampLux = 0;
+  if (!isfinite(clampLux) || clampLux < 0) clampLux = 0;
   float normalized = clampLux / MAX_LUX_FOR_PWM;
   if (normalized > 1.0) normalized = 1.0;
   int pwmValue = (int)round(normalized * 255.0);
@@ -39,7 +48,13 @@ void loop() {
   if (pwmValue > 255) pwmValue = 255;
 
   // ---- Numeric line for Serial Plotter ----
-  Serial.print(rawALS);
-  Serial.print(' ');
-  Serial.println(pwmValue);
+  lcd.setCursor(0,1);
+  lcd.print(rawALS);
+
+  lcd.setCursor(15,1);
+  lcd.print(pwmValue);
+
+  // Serial.print(rawALS);
+  // Serial.print(' ');
+  // Serial.println(pwmValue);
 }
